385eccfc23c0e345b0949b4d37c02b7474dbcdc7
georgekaran <georgekaran12@gmail.com>
2020-09-13 19:29:06 -0300

    refactor: move sut creation to its own factory

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, RenderResult, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 
 import Logo from './Logo'
 import theme from '@/styles/theme'
 
+const makeSut = (): RenderResult => {
+  return render(
+    <ThemeProvider theme={theme}>
+      <Logo />
+    </ThemeProvider>
+  )
+}
+
 describe('<Logo />', () => {
   test('should render correctly', () => {
-    const { container } = render(
-      <ThemeProvider theme={theme}>
-        <Logo />
-      </ThemeProvider>
-    )
+    const { container } = makeSut()
     expect(screen.getByTestId('logo')).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
   })
