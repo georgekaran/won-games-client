cee28a6a980502d2ee4babbe61bd9034c66e5122
georgekaran <georgekaran12@gmail.com>
2020-09-13 19:49:50 -0300

    test: ensure Logo changes text color

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import { render, RenderResult, screen } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 
-import Logo from './Logo'
+import Logo, { LogoProps } from './Logo'
 import theme from '@/styles/theme'
 
-const makeSut = (): RenderResult => {
+const makeSut = ({ color = 'white' }: LogoProps = {}): RenderResult => {
   return render(
     <ThemeProvider theme={theme}>
-      <Logo />
+      <Logo color={color} />
     </ThemeProvider>
   )
 }
@@ -18,5 +18,10 @@ describe('<Logo />', () => {
     const { container } = makeSut()
     expect(screen.getByTestId('logo')).toBeInTheDocument()
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('should change text color', () => {
+    makeSut({ color: 'black' })
+    expect(screen.getByTestId('logo')).toHaveStyleRule('color', theme.colors.black)
   })
 })
