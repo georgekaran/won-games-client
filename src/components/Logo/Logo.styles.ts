0b2e9b3d324fe42f1fa8f243ac26219cb311901c
georgekaran <georgekaran12@gmail.com>
2020-09-14 17:48:49 -0300

    feat: ensure Logo hides text if hideOnMobile is true and max-width is 768px

--- src/components/Logo/Logo.styles.ts
+++ src/components/Logo/Logo.styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 import { LogoProps } from './Logo'
 
@@ -10,12 +11,28 @@ const wrapperModifiers = {
   normal: () => css`
     width: 11rem;
     height: 3.3rem;
+  `,
+  hideOnMobile: () => css`
+    ${media.lessThan('medium')`
+      width: 5.8rem;
+      height: 4.5rem;
+
+      svg {
+        height: 4.5rem;
+        pointer-events: none;
+      }
+
+      .logo-text {
+        display: none;
+      }
+    `}
   `
 }
 
 export const Wrapper = styled.div<LogoProps>`
-  ${({ theme, color, size }) => css`
+  ${({ theme, color, size, hideOnMobile }) => css`
     color: ${theme.colors[color!]};
     ${wrapperModifiers[size!]()}
+    ${!!hideOnMobile && wrapperModifiers.hideOnMobile()}
   `}
 `
