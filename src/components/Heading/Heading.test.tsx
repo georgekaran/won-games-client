3d0f0820ded067a1895a651a19284492d3fdba06
georgekaran <georgekaran12@gmail.com>
2020-09-15 09:24:23 -0300

    test: ensure Heading change color to black if color is provided

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -3,19 +3,25 @@ import { RenderResult, screen } from '@testing-library/react'
 
 import Heading, { HeadingProps } from './Heading'
 import { renderWithTheme } from '@/test/helpers'
+import theme from '@/styles/theme'
 
-const makeSut = ({ children = 'Random Heading' }: HeadingProps = { children: 'Random Heading' }): RenderResult => {
-  return renderWithTheme(<Heading>{children}</Heading>)
+const makeSut = ({ children, color = 'white' }: HeadingProps): RenderResult => {
+  return renderWithTheme(<Heading color={color}>{children}</Heading>)
 }
 
 describe('<Heading />', () => {
   test('should render with initial state', () => {
-    const { container } = makeSut()
+    const { container } = makeSut({ children: 'Random Heading' })
     const heading = screen.getByRole('heading', { name: /Random Heading/ })
     expect(heading).toBeInTheDocument()
-    expect(heading).toHaveStyleRule('color', '#FAFAFA')
+    expect(heading).toHaveStyleRule('color', theme.colors.white)
     expect(heading).not.toHaveStyleRule('padding-left')
     expect(heading).not.toHaveStyleRule('padding-bottom')
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  test('should render Heading with black text if color black is provided', () => {
+    makeSut({ children: 'Random Heading', color: 'black' })
+    expect(screen.getByRole('heading', { name: /Random Heading/ })).toHaveStyleRule('color', theme.colors.black)
   })
 })
