454791bee34c0ddd28976163a866357812ac49c7
georgekaran <georgekaran12@gmail.com>
2020-09-15 17:20:33 -0300

    test: ensure Button render with correct initial state

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Button from './Button'
+import { renderWithTheme } from '@/test/helpers'
+import theme from '@/styles/theme'
 
 describe('<Button />', () => {
-  it('should render with initial state', () => {
-    const { container } = render(<Button />)
+  test('should render with correct initial state', () => {
+    const { container } = renderWithTheme(<Button>Button</Button>)
     expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('button', { name: /Button/ })).toHaveStyleRule('height', '4rem')
+    expect(screen.getByRole('button', { name: /Button/ })).toHaveStyleRule('font-size', theme.font.sizes.small)
+    expect(screen.getByRole('button', { name: /Button/ })).toHaveStyleRule('padding', `${theme.spacings.xxsmall} ${theme.spacings.medium}`)
   })
 })
