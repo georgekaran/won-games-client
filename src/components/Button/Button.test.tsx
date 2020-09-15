da1ff7fce92ffeb0f5fe339e53771b6ebe361c8a
georgekaran <georgekaran12@gmail.com>
2020-09-15 17:27:27 -0300

    refactor: move sut creation to a factory method

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { RenderResult, screen } from '@testing-library/react'
 
-import Button from './Button'
+import Button, { ButtonProps } from './Button'
 import { renderWithTheme } from '@/test/helpers'
 import theme from '@/styles/theme'
 
+type SutProps = Pick<ButtonProps, 'size'>
+
+const makeSut = ({ size = 'medium' }: SutProps = {}): RenderResult => {
+  return renderWithTheme(<Button>Button</Button>)
+}
+
 describe('<Button />', () => {
   test('should render with correct initial state', () => {
-    const { container } = renderWithTheme(<Button>Button</Button>)
+    const { container } = makeSut()
     expect(container.firstChild).toMatchSnapshot()
     const button = screen.getByRole('button', { name: /Button/ })
     expect(button).toHaveStyleRule('height', '4rem')
