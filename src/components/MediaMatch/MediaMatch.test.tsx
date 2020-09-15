3302d31331b1eaa2e24d9092d7cd15073d400048
georgekaran <georgekaran12@gmail.com>
2020-09-15 15:14:36 -0300

    test: ensure MediaMatch hide content if no media query is provided

---
+++ src/components/MediaMatch/MediaMatch.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import MediaMatch from './MediaMatch'
+
+describe('<MediaMatch />', () => {
+  let desktopHeading: Element
+  let mobileHeading: Element
+
+  beforeEach(() => {
+    render(
+      <>
+        <MediaMatch greaterThan="medium">
+          <h1 data-testid="desktop">Desktop</h1>
+        </MediaMatch>
+        <MediaMatch lessThan="medium">
+          <h1 data-testid="mobile">Desktop</h1>
+        </MediaMatch>
+      </>
+    )
+
+    desktopHeading = screen.getByTestId('desktop')
+    mobileHeading = screen.getByTestId('mobile')
+  })
+
+  test('should be hidden if no media query is passed', () => {
+    expect(desktopHeading.parentElement).toHaveStyleRule('display', 'none')
+    expect(mobileHeading.parentElement).toHaveStyleRule('display', 'none')
+  })
+})
