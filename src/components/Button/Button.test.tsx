0153ede3a6acb373ba189266fafdc8706b529b86
georgekaran <georgekaran12@gmail.com>
2020-09-15 17:22:00 -0300

    refactor: remove duplicate code

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -9,8 +9,9 @@ describe('<Button />', () => {
   test('should render with correct initial state', () => {
     const { container } = renderWithTheme(<Button>Button</Button>)
     expect(container.firstChild).toMatchSnapshot()
-    expect(screen.getByRole('button', { name: /Button/ })).toHaveStyleRule('height', '4rem')
-    expect(screen.getByRole('button', { name: /Button/ })).toHaveStyleRule('font-size', theme.font.sizes.small)
-    expect(screen.getByRole('button', { name: /Button/ })).toHaveStyleRule('padding', `${theme.spacings.xxsmall} ${theme.spacings.medium}`)
+    const button = screen.getByRole('button', { name: /Button/ })
+    expect(button).toHaveStyleRule('height', '4rem')
+    expect(button).toHaveStyleRule('font-size', theme.font.sizes.small)
+    expect(button).toHaveStyleRule('padding', `${theme.spacings.xxsmall} ${theme.spacings.medium}`)
   })
 })
