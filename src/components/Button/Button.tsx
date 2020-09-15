7fbfcb04d91cf747e1fae7c5a2dbf138c9d69d12
georgekaran <georgekaran12@gmail.com>
2020-09-15 17:49:43 -0300

    feat: ensure Button renders full width if prop is provided

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,10 +4,11 @@ import * as S from './Button.styles'
 export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
 }
 
-const Button = ({ children, size = 'medium' }: ButtonProps) => (
-  <S.Wrapper size={size}>
+const Button = ({ children, size = 'medium', fullWidth = false }: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth}>
     {!!children && <span>{children}</span>}
   </S.Wrapper>
 )
