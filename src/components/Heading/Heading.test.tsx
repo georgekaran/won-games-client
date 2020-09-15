b3a13f62a255a45a63ffb7cc27bc93d22d3b4428
georgekaran <georgekaran12@gmail.com>
2020-09-15 09:29:11 -0300

    test: ensure Heading present line in the left if lineLeft is true

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -5,8 +5,8 @@ import Heading, { HeadingProps } from './Heading'
 import { renderWithTheme } from '@/test/helpers'
 import theme from '@/styles/theme'
 
-const makeSut = ({ children, color = 'white' }: HeadingProps): RenderResult => {
-  return renderWithTheme(<Heading color={color}>{children}</Heading>)
+const makeSut = ({ children, color = 'white', lineLeft = false }: HeadingProps): RenderResult => {
+  return renderWithTheme(<Heading color={color} lineLeft={lineLeft}>{children}</Heading>)
 }
 
 describe('<Heading />', () => {
@@ -23,5 +23,12 @@ describe('<Heading />', () => {
   test('should render Heading with black text if color black is provided', () => {
     makeSut({ children: 'Random Heading', color: 'black' })
     expect(screen.getByRole('heading', { name: /Random Heading/ })).toHaveStyleRule('color', theme.colors.black)
+  })
+
+  test('should present a line in the left if lineLeft is true', () => {
+    makeSut({ children: 'Random Heading', lineLeft: true })
+    const heading = screen.getByRole('heading', { name: /Random Heading/ })
+    expect(heading).toHaveStyleRule('padding-left', theme.spacings.xxsmall)
+    expect(heading).toHaveStyleRule('border-left', `0.7rem solid ${theme.colors.secondary}`)
   })
 })
