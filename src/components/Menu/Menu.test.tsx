7954f601f3521bfc01493aecf3d5093c35411d96
georgekaran <georgekaran12@gmail.com>
2020-09-16 12:08:24 -0300

    test: ensure Menu handle open/close correctly

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { screen, RenderResult } from '@testing-library/react'
+import { screen, RenderResult, fireEvent } from '@testing-library/react'
 
 import Menu from './Menu'
 import { renderWithTheme } from '@/test/helpers'
@@ -16,5 +16,24 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
     expect(screen.getByTestId('logo')).toBeInTheDocument()
+  })
+
+  test('should handle the open/close mobile menu', () => {
+    makeSut()
+    // Select the menu
+    const menuFull = screen.getByRole('navigation', { hidden: true })
+    // Check if menu is hide
+    expect(menuFull.getAttribute('aria-hidden')).toBe('true')
+    expect(menuFull).toHaveStyleRule('opacity', '0')
+    // Click in the button to open the menu e check if the menu is open
+    const menuIcon = screen.getByLabelText(/open menu/i)
+    fireEvent.click(menuIcon)
+    expect(menuFull.getAttribute('aria-hidden')).toBe('false')
+    expect(menuFull).toHaveStyleRule('opacity', '1')
+    // Click in the button to close the menu e check if the menu is hide/close
+    const closeMenuIcon = screen.getByLabelText(/close menu/i)
+    fireEvent.click(closeMenuIcon)
+    expect(menuFull.getAttribute('aria-hidden')).toBe('true')
+    expect(menuFull).toHaveStyleRule('opacity', '0')
   })
 })
