57e3dc843cea10a8b8700b6ce4da1fd3612b20f8
georgekaran <georgekaran12@gmail.com>
2020-09-16 11:39:23 -0300

    refactor: move sut creation to a factory method

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { screen } from '@testing-library/react'
+import { screen, RenderResult } from '@testing-library/react'
 
 import Menu from './Menu'
 import { renderWithTheme } from '@/test/helpers'
 
+const makeSut = (): RenderResult => {
+  return renderWithTheme(<Menu />)
+}
+
 describe('<Menu />', () => {
   test('should render with initial state', () => {
-    const { container } = renderWithTheme(<Menu />)
+    const { container } = makeSut()
     expect(container.firstChild).toMatchSnapshot()
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
