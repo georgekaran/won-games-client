b7abcf2a6df441b8cda3261c19586dd70d02c4f6
georgekaran <georgekaran12@gmail.com>
2020-09-16 10:08:31 -0300

    feat: add Menu Component

---
+++ src/components/Menu/Menu.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Menu from './Menu'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+export const Default: Story = () => <Menu />
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './Menu.styles'
+
+export type MenuProps = {}
+
+const Menu = () => (
+  <S.Wrapper>
+    <h1>Menu</h1>
+  </S.Wrapper>
+)
+
+export default Menu
