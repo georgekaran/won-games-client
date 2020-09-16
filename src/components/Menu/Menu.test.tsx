21e69788942919bcafa056d05d3eb084e8e8df0b
georgekaran <georgekaran12@gmail.com>
2020-09-16 11:36:44 -0300

    test: ensure Menu render with correct initial state

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Menu from './Menu'
+import { renderWithTheme } from '@/test/helpers'
 
 describe('<Menu />', () => {
   test('should render with initial state', () => {
-    const { container } = render(<Menu />)
+    const { container } = renderWithTheme(<Menu />)
     expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+    expect(screen.getByTestId('logo')).toBeInTheDocument()
   })
 })
