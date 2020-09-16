2fb2b1bbeb2a9eb40569d75a7d3358b0affdf45a
georgekaran <georgekaran12@gmail.com>
2020-09-16 12:08:12 -0300

    feat: ensure Menu handle open/close correctly

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,30 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-line/Menu2'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
 import { ShoppingCart } from '@styled-icons/material-outlined/ShoppingCart'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import * as S from './Menu.styles'
 import Logo from '@/components/Logo/Logo'
 
 export type MenuProps = {}
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile />
-    </S.LogoWrapper>
-    <S.MenuGroup>
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <S.Wrapper>
       <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
+        <MenuIcon aria-label="Open menu" onClick={() => setIsOpen(true)} />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <ShoppingCart aria-label="Open shopping cart" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <ShoppingCart aria-label="Open shopping cart" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
