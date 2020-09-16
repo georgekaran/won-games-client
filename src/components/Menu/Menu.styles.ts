7accb129c0361baf9c2cd97a71f1c038ce93afd7
georgekaran <georgekaran12@gmail.com>
2020-09-16 11:30:34 -0300

    feat: add icons to Menu

--- src/components/Menu/Menu.styles.ts
+++ src/components/Menu/Menu.styles.ts
@@ -1,3 +1,40 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.menu`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    padding: ${theme.spacings.small} 0;
+    position: relative;
+  `}
+`
+
+export const IconWrapper = styled.div`
+  ${({ theme }) => css`
+    color: ${theme.colors.white};
+    width: ${theme.spacings.small};
+    height: ${theme.spacings.small};
+    cursor: pointer;
+  `}
+`
+
+export const LogoWrapper = styled.div`
+  ${media.lessThan('medium')`
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  `}
+`
+
+export const MenuGroup = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-grow: 1;
+    justify-content: flex-end;
+  
+    > div {
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-line/Menu2'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { ShoppingCart } from '@styled-icons/material-outlined/ShoppingCart'
+
 import * as S from './Menu.styles'
+import Logo from '@/components/Logo/Logo'
 
 export type MenuProps = {}
 
 const Menu = () => (
   <S.Wrapper>
-    <h1>Menu</h1>
+    <S.IconWrapper>
+      <MenuIcon />
+    </S.IconWrapper>
+    <S.LogoWrapper>
+      <Logo hideOnMobile />
+    </S.LogoWrapper>
+    <S.MenuGroup>
+      <S.IconWrapper>
+        <SearchIcon />
+      </S.IconWrapper>
+      <S.IconWrapper>
+        <ShoppingCart />
+      </S.IconWrapper>
+    </S.MenuGroup>
   </S.Wrapper>
 )
 
