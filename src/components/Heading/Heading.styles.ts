3bbb96f4e15f4bb639d2a383df397463c2023bf9
georgekaran <georgekaran12@gmail.com>
2020-09-17 12:12:09 -0300

    feat: ensure Heading change lineColor is prop is provided

--- src/components/Heading/Heading.styles.ts
+++ src/components/Heading/Heading.styles.ts
@@ -1,14 +1,14 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 
-import { HeadingProps } from './Heading'
+import { HeadingProps, LineColors } from './Heading'
 
 const wrapperModifiers = {
-  lineLeft: (theme: DefaultTheme) => css`
+  lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xxsmall};
-    border-left: 0.7rem solid ${theme.colors.secondary};
+    border-left: 0.7rem solid ${theme.colors[lineColor]};
   `,
-  lineBottom: (theme: DefaultTheme) => css`
+  lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
 
@@ -18,7 +18,7 @@ const wrapperModifiers = {
       left: 0;
       bottom: -0.5rem;
       width: 5rem;
-      background-color: ${theme.colors.primary};
+      background-color: ${theme.colors[lineColor]};
       height: 0.7rem;
     }
   `,
@@ -39,11 +39,11 @@ const wrapperModifiers = {
 }
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom, size }) => css`
+  ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
     color: ${theme.colors[color!]};
 
-    ${lineLeft && wrapperModifiers.lineLeft(theme)}
-    ${lineBottom && wrapperModifiers.lineBottom(theme)}
+    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
     ${!!size && wrapperModifiers[size](theme)}
   `}
 `
--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import * as S from './Heading.styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
   size?: 'medium' | 'small'
+  lineColor?: LineColors
 }
 
-const Heading = ({ children, color = 'white', lineLeft = false, lineBottom = false, size = 'medium' }: HeadingProps) => (
+const Heading = ({ children, color = 'white', lineLeft = false, lineBottom = false, size = 'medium', lineColor = 'primary' }: HeadingProps) => (
   <S.Wrapper
     color={color}
     lineLeft={lineLeft}
     lineBottom={lineBottom}
-    size={size}>
+    size={size}
+    lineColor={lineColor}>
     {children}
   </S.Wrapper>
 )
