30781dc679ddb37b7b63a023c6de2ab887ce85a8
georgekaran <georgekaran12@gmail.com>
2020-09-17 12:12:29 -0300

    test: ensure Heading change lineColor is prop is provided

--- src/components/Heading/Heading.test.tsx
+++ src/components/Heading/Heading.test.tsx
@@ -7,13 +7,14 @@ import theme from '@/styles/theme'
 
 type SutProps = Omit<HeadingProps, 'children'>
 
-const makeSut = ({ color = 'white', lineLeft = false, lineBottom = false, size = 'medium' }: SutProps): RenderResult => {
+const makeSut = ({ color = 'white', lineLeft = false, lineBottom = false, size = 'medium', lineColor = 'primary' }: SutProps): RenderResult => {
   return renderWithTheme(
     <Heading
       color={color}
       lineLeft={lineLeft}
       lineBottom={lineBottom}
-      size={size}>
+      size={size}
+      lineColor={lineColor}>
       Random Heading
     </Heading>)
 }
@@ -38,7 +39,7 @@ describe('<Heading />', () => {
     makeSut({ lineLeft: true })
     const heading = screen.getByRole('heading', { name: /Random Heading/ })
     expect(heading).toHaveStyleRule('padding-left', theme.spacings.xxsmall)
-    expect(heading).toHaveStyleRule('border-left', `0.7rem solid ${theme.colors.secondary}`)
+    expect(heading).toHaveStyleRule('border-left', `0.7rem solid ${theme.colors.primary}`)
   })
 
   test('should present a line in the bottom if lineBottom is true', () => {
@@ -66,5 +67,33 @@ describe('<Heading />', () => {
         modifier: '::after'
       }
     )
+  })
+
+  test('should change Heading line color to primary', () => {
+    makeSut({ lineBottom: true, lineLeft: true, lineColor: 'primary' })
+    const heading = screen.getByRole('heading', { name: /Random Heading/ })
+    expect(heading).toHaveStyle({
+      'border-left': `0.7rem solid ${theme.colors.primary}`
+    })
+    expect(heading).toHaveStyleRule(
+      'background-color',
+      theme.colors.primary,
+      {
+        modifier: '::after'
+      })
+  })
+
+  test('should change Heading line color to secondary', () => {
+    makeSut({ lineBottom: true, lineLeft: true, lineColor: 'secondary' })
+    const heading = screen.getByRole('heading', { name: /Random Heading/ })
+    expect(heading).toHaveStyle({
+      'border-left': `0.7rem solid ${theme.colors.secondary}`
+    })
+    expect(heading).toHaveStyleRule(
+      'background-color',
+      theme.colors.secondary,
+      {
+        modifier: '::after'
+      })
   })
 })
