5897747aa4ee807a2ab3b2a6e19c51080fca3c59
georgekaran <georgekaran12@gmail.com>
2020-09-17 11:52:51 -0300

    fix: remove fallback font-size from Heading

--- src/components/Heading/Heading.styles.ts
+++ src/components/Heading/Heading.styles.ts
@@ -36,12 +36,7 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.h2<HeadingProps>`
   ${({ theme, color, lineLeft, lineBottom, size }) => css`
-    font-size: ${theme.font.sizes.xlarge};
     color: ${theme.colors[color!]};
-
-    ${media.greaterThan('medium')`
-      font-size: ${theme.font.sizes.xxlarge};
-    `}
 
     ${lineLeft && wrapperModifiers.lineLeft(theme)}
     ${lineBottom && wrapperModifiers.lineBottom(theme)}
