231b9a37cca49e09be7bf9244404e2eb23ec63da
georgekaran <georgekaran12@gmail.com>
2020-09-20 17:56:58 -0300

    feat: ensure Ribbon renders with normal size as default

--- src/components/Ribbon/Ribbon.styles.ts
+++ src/components/Ribbon/Ribbon.styles.ts
@@ -5,11 +5,19 @@ import { RibbonColors, RibbonProps } from '.'
 const wrapperModifiers = {
   color: (theme: DefaultTheme, color: RibbonColors) => css`
     background-color: ${theme.colors[color]}
+  `,
+  normal: (theme: DefaultTheme) => css`
+    height: 3.6rem;
+    font-size: ${theme.font.sizes.small};
+  `,
+  small: (theme: DefaultTheme) => css`
+    
   `
 }
 
 export const Wrapper = styled.div<Omit<RibbonProps, 'children'>>`
-  ${({ theme, color }) => css`
-    ${!!color && wrapperModifiers.color(theme, color)}
+  ${({ theme, color, size }) => css`
+    ${!!color && wrapperModifiers.color(theme, color)};
+    ${!!size && wrapperModifiers[size](theme)};
   `}
 `
--- src/components/Ribbon/Ribbon.tsx
+++ src/components/Ribbon/Ribbon.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import * as S from './Ribbon.styles'
 
 export type RibbonColors = 'primary' | 'secondary'
+export type RibbonSizes = 'normal' | 'small'
 
 export type RibbonProps = {
   children: React.ReactNode
   color?: RibbonColors
+  size?: RibbonSizes
 }
 
-const Ribbon = ({ children, color = 'primary' }: RibbonProps) => (
-  <S.Wrapper color={color}>
+const Ribbon = ({ children, color = 'primary', size = 'normal' }: RibbonProps) => (
+  <S.Wrapper color={color} size={size}>
     {children}
   </S.Wrapper>
 )
