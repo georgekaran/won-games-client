7ffa28beccc9ebc68df54bedba77e4134e65b2f8
georgekaran <georgekaran12@gmail.com>
2020-09-21 15:31:55 -0300

    feat: ensure Highlight renders button link

--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import * as S from './Highlight.styles'
 
+import { Button } from '@/components/Button'
+
 export type HighlightProps = {
   title: string
   subtitle: string
+  buttonLabel: string
+  buttonLink: string
 }
 
-const Highlight = ({ title, subtitle }: HighlightProps) => (
+const Highlight = ({ title, subtitle, buttonLabel, buttonLink }: HighlightProps) => (
   <S.Wrapper>
     <S.Title>{title}</S.Title>
     <S.Subtitle>{subtitle}</S.Subtitle>
+    <Button as="a" href={buttonLink}>{buttonLabel}</Button>
   </S.Wrapper>
 )
 
