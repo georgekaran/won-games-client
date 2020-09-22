41a50c65cef2b673502f4c79cc961f05b5d36e6b
georgekaran <georgekaran12@gmail.com>
2020-09-21 22:12:53 -0300

    feat: ensure Highlight renders a background image

--- src/components/Highlight/Highlight.stories.tsx
+++ src/components/Highlight/Highlight.stories.tsx
@@ -10,7 +10,8 @@ export default {
     title: 'Read Dead it\'s back',
     subtitle: 'Come see John\'s new adventures',
     buttonLabel: 'Buy now',
-    buttonLink: '/rdr2'
+    buttonLink: '/rdr2',
+    backgroundImage: 'https://alexanderskogberg.com/wp-content/uploads/2018/12/featured-read-dead-redemption-2-1400x640.jpg'
   }
 } as Meta
 
--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -8,13 +8,16 @@ export type HighlightProps = {
   subtitle: string
   buttonLabel: string
   buttonLink: string
+  backgroundImage: string
 }
 
-const Highlight = ({ title, subtitle, buttonLabel, buttonLink }: HighlightProps) => (
-  <S.Wrapper>
-    <S.Title>{title}</S.Title>
-    <S.Subtitle>{subtitle}</S.Subtitle>
-    <Button as="a" href={buttonLink}>{buttonLabel}</Button>
+const Highlight = ({ title, subtitle, buttonLabel, buttonLink, backgroundImage }: HighlightProps) => (
+  <S.Wrapper backgroundImage={backgroundImage}>
+    <S.Content>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+      <Button as="a" href={buttonLink}>{buttonLabel}</Button>
+    </S.Content>
   </S.Wrapper>
 )
 
