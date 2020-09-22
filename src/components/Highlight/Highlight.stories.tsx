0e7102b26fa643514fbf38a1b805cfaeb4d97f90
georgekaran <georgekaran12@gmail.com>
2020-09-21 21:44:29 -0300

    feat: add default args to Highlight story

--- src/components/Highlight/Highlight.stories.tsx
+++ src/components/Highlight/Highlight.stories.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Highlight from './Highlight'
+
+import { Highlight, HighlightProps } from '.'
 
 export default {
   title: 'Highlight',
-  component: Highlight
+  component: Highlight,
+  args: {
+    title: 'Read Dead it\'s back',
+    subtitle: 'Come see John\'s new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
 } as Meta
 
-export const Default: Story = () => <Highlight />
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
