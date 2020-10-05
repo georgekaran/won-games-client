bf2eb8f72d27180300b2c496c2a0a5c184323577
georgekaran <georgekaran12@gmail.com>
2020-10-04 21:19:15 -0300

    test: ensure BannerSlider render vertical slider

---
+++ src/components/BannerSlider/BannerSlider.test.tsx
@@ -0,0 +1,30 @@
+import '@/test/match-media-mock'
+import React from 'react'
+
+import { BannerSlider } from '.'
+import { BannerProps } from '@/components/Banner/Banner'
+import { renderWithTheme } from '@/test/helpers'
+
+const items: BannerProps[] = [
+  {
+    img: 'https://gamespot1.cbsistatic.com/uploads/screen_kubrick/1591/15918215/3690642-cyberpunk.jpg',
+    title: 'Cyberpunk 2077',
+    subtitle: '<p>Play the Cyberpunk 2077 now!</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/cyberpunk-2077'
+  },
+  {
+    img: 'https://www.gamerview.com.br/wp-content/uploads/2018/11/red_dead_redemption_2_bg-e1557756295124.jpg',
+    title: 'Red Dead Redemption 2',
+    subtitle: '<p>Play the Red Dead Redemption 2 now!</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/red-dead-redemption-2'
+  }
+]
+
+describe('<BannerSlider />', () => {
+  it('should render vertical slider', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+    expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+  })
+})
