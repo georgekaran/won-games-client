bd831ad1cb62ac65255f341b3c738cec5abe38b4
georgekaran <georgekaran12@gmail.com>
2020-11-08 11:21:29 -0300

    test: ensure GameCardSlider render white arrows if color prop is provided

--- src/components/GameCardSlider/GameCardSlider.test.tsx
+++ src/components/GameCardSlider/GameCardSlider.test.tsx
@@ -1,5 +1,6 @@
 import '@/test/match-media-mock'
 import React from 'react'
+import { screen } from '@testing-library/react'
 
 import { renderWithTheme } from '@/test/helpers'
 import { GameCardSlider } from '.'
@@ -54,5 +55,16 @@ describe('<GameSlider />', () => {
   it('should render with 4 active items', () => {
     const { container } = renderWithTheme(<GameCardSlider items={items} />)
     expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+  })
+
+  it('should render white arrows if color passed', () => {
+    renderWithTheme(<GameCardSlider items={items} color="white" />)
+
+    expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
+    expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+      color: '#FAFAFA'
+    })
   })
 })
