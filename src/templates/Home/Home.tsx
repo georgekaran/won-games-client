fb3a99719f3c264adc69cb9a018453d85ebe040a
georgekaran <georgekaran12@gmail.com>
2020-11-22 16:45:03 -0300

    feat(Home): ensure renders heading sections

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Container } from '@/components/Container'
 import { Footer } from '@/components/Footer'
 import { Menu } from '@/components/Menu'
+import { Heading } from '@/components/Heading'
 
 export type HomeProps = {}
 
@@ -10,6 +11,30 @@ const Home = () => (
   <section>
     <Container>
       <Menu />
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        News
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Most Popular
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Upcomming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Free Games
+      </Heading>
     </Container>
 
     <Container>
