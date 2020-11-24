d1148c5e2715a7fe4fc5e820219171c7c818c84d
georgekaran <georgekaran12@gmail.com>
2020-11-24 15:54:45 -0300

    feat: add mock data to Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
-import Home from '@/templates/Home'
+import Home, { HomeProps } from '@/templates/Home'
+import bannerMock from '@/components/BannerSlider/mock'
+import gameCardSliderMock from '@/components/GameCardSlider/mock'
+import highlightMock from '@/components/Highlight/mock'
 
-const Index = () => {
+const Index = (props: HomeProps) => {
   return (
-    <Home />
+    <Home {...props} />
   )
+}
+
+export function getServerSideProps () {
+  return {
+    props: {
+      banners: bannerMock,
+      newGamesSlider: gameCardSliderMock,
+      mostPopularHighlight: highlightMock,
+      mostPopularGames: gameCardSliderMock,
+      upcomingGames: gameCardSliderMock,
+      upcomingGamesHighlight: highlightMock,
+      upcomingMoreGames: gameCardSliderMock,
+      freeGames: gameCardSliderMock,
+      freeHighlight: highlightMock
+    }
+  }
 }
 
 export default Index
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -4,37 +4,71 @@ import { Container } from '@/components/Container'
 import Footer from '@/components/Footer'
 import Menu from '@/components/Menu'
 import Heading from '@/components/Heading'
+import { BannerProps } from '@/components/Banner'
+import { GameCardProps } from '@/components/GameCard'
+import Highlight, { HighlightProps } from '@/components/Highlight'
+import BannerSlider from '@/components/BannerSlider'
+import GameCardSlider from '@/components/GameCardSlider'
 
-export type HomeProps = {}
+export type HomeProps = {
+  banners: BannerProps[]
+  newGamesSlider: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcomingGames: GameCardProps[]
+  upcomingGamesHighlight: HighlightProps
+  upcomingMoreGames: GameCardProps[]
+  freeGames: GameCardProps[]
+  freeHighlight: HighlightProps
+}
 
-const Home = () => (
+const Home = ({
+  banners,
+  newGamesSlider,
+  mostPopularHighlight,
+  freeGames,
+  freeHighlight,
+  mostPopularGames,
+  upcomingGames,
+  upcomingGamesHighlight,
+  upcomingMoreGames
+}: HomeProps) => (
   <section>
     <Container>
       <Menu />
+      <BannerSlider items={banners} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary" color="black">
         News
       </Heading>
+      <GameCardSlider items={newGamesSlider} color="black" />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary">
         Most Popular
       </Heading>
+      <Highlight {...mostPopularHighlight} />
+      <GameCardSlider items={mostPopularGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary">
         Upcomming
       </Heading>
+      <GameCardSlider items={upcomingGames} />
+      <Highlight {...upcomingGamesHighlight} />
+      <GameCardSlider items={upcomingMoreGames} />
     </Container>
 
     <Container>
       <Heading lineLeft lineColor="secondary">
         Free Games
       </Heading>
+      <Highlight {...freeHighlight} />
+      <GameCardSlider items={freeGames} />
     </Container>
 
     <Container>
@@ -42,9 +76,5 @@ const Home = () => (
     </Container>
   </section>
 )
-
-export function getStaticProps () {
-
-}
 
 export default Home
