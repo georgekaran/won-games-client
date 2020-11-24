f1ef8c3824490e49c8aab255346c3ab138e425a2
georgekaran <georgekaran12@gmail.com>
2020-11-24 09:02:21 -0300

    fix: fix unmatch imports from stories files

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
-import { GameCard, GameCardProps } from '.'
+import GameCard, { GameCardProps } from '.'
 
 export default {
   title: 'GameCard',
